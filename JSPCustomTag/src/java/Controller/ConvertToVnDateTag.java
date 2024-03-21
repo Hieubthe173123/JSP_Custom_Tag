91c2bc0c0ed5c56c61e144adf3ad00e2eab5c4b8
KienPD28 <kienpdhe170155@fpt.edu.vn>
2024-03-21 21:28:31 +0700

    JSP Custom Tag

---
+++ JSPCustomTag/src/java/Controller/ConvertToVnDateTag.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/TagHandler.java to edit this template
+ */
+package Controller;
+
+import jakarta.servlet.jsp.JspWriter;
+import jakarta.servlet.jsp.JspException;
+import jakarta.servlet.jsp.tagext.JspFragment;
+import jakarta.servlet.jsp.tagext.SimpleTagSupport;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ *
+ * @author DELL
+ */
+public class ConvertToVnDateTag extends SimpleTagSupport {
+
+    private String value;
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public void doTag() throws JspException, IOException {
+        String formattedDate = convertDate(value);
+        getJspContext().getOut().write(formattedDate);
+    }
+    
+    private String convertDate(String value){
+        try {
+            //format ngày ban đầu
+            SimpleDateFormat inputFormatDate = new SimpleDateFormat("MM-dd-yyyy");
+            //format ngày mới
+            SimpleDateFormat outputFormatDate = new SimpleDateFormat("'Ngay' dd, 'thang' MM, 'nam' yyyy");
+            
+            //Convert formart date ban đầu
+            Date date = inputFormatDate.parse(value);
+            
+            //out put ra format date mới
+            String formatDate = outputFormatDate.format(date);
+            
+            return formatDate;
+            
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+}
---
+++ JSPCustomTag/web/TagCustom.jsp
@@ -0,0 +1,20 @@
+<%-- 
+    Document   : TagCustom
+    Created on : Mar 21, 2024, 8:40:26 PM
+    Author     : DELL
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="WEB-INF/tlds/TagCustom.tld"%>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Date Format</h1>
+        <c:ConvertToVnDateTag value="05-15-2024"/>
+    </body>
+</html>
